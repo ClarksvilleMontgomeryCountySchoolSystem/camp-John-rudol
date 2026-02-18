f309da8ae0c50843ed4b09b0a929b44dafbec7bc
John-rudol <johnathan.rudolph097@students.cmcss.net>
2026-02-18 07:39:36 -0600

    a

--- task2.py
+++ task2.py
@@ -7,34 +7,34 @@ from data import first, last, birth, days, p_first, p_last, phone, street, city,
 
 
 # first
-
+print(type(first))
 
 # last
-
+print(type(last))
 
 # birth
-
+print(type(birth))
 
 # days
-
-
+print(type(days))
+days = int(days)
 # p_first
-
+print(type(p_first))
 
 # p_last
-
+print(type(p_last))
 
 # phone
-
+print(type(phone))
 
 # street
-
+print(type(street))
 
 # city
-
+print(type(city))
 
 # state
-
+print(type(state))
 
 # zip_code
-
+print(type(zip_code))
